21c9b301be6b464989c5107989e7f202ec170c32
joeyharbert <joey.harbert@gmail.com>
2019-12-15 11:23:31 -0600

    Adds functionality so that start_date is always a monday, and creation of water_days as part of calendar create

--- app/controllers/api/calendars_controller.rb
+++ app/controllers/api/calendars_controller.rb
@@ -14,17 +14,48 @@ class Api::CalendarsController < ApplicationController
   def create
       #create calendar
       if params[:start_date]
-        @calendar = Calendar.create(start_date: params[:start_date])
-        
-        #create water_days attached to calendar
+        #check if start_date is a monday, if not, make it a monday
+        start_date = Date.parse(params[:start_date])
+        if start_date.cwday != 1
+          entered_date_number = start_date.cwday
 
+          # increment date until monday (we get to 1 if the entered date was a sunday, otherwise we get to 8)
+          while entered_date_number < 1 || entered_date_number < 8
+            start_date += 1
+            entered_date_number += 1
+          end       
+        end
 
-        #create all plants (this will be given to us from front end)
-          #for now, we will just use all plants in database
-
-        #calculate which plants for which days and attach
+        @calendar = Calendar.create(start_date: start_date)
 
         if @calendar.save
+          #create water_days attached to calendar
+          #make number of weeks for calendar scalable
+          num_of_weeks = 0
+          if params[:num_of_weeks]
+            num_of_weeks = params[:num_of_weeks].to_i
+            if num_of_weeks == 0
+              render json: {error: "Please enter a valid number of weeks for the calendar"}
+              return
+            end
+          else
+            num_of_weeks = 12
+          end
+
+          num_of_days = num_of_weeks * 7
+          current_date = start_date
+
+          num_of_days.times do 
+            WaterDay.create(date: current_date, calendar_id: @calendar.id)
+            current_date += 1
+          end
+
+          #create all plants (this will be given to us from front end)
+            #for now, we will just use all plants in database
+
+          #calculate which plants for which days and attach
+
+
           render 'show.json.jbuilder'
         else
           render json: {errors: @plant.errors.full_messages}, status: :bad_request
